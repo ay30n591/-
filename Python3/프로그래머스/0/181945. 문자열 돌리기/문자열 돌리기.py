3be099f36c4c46228799bcbe5ce94c8cbcc3ad24
SuHyeon <132035053+ay30n591@users.noreply.github.com>
2024-02-19 21:38:12 +0900

    [level 0] Title: 문자열 돌리기, Time: 10.66 ms, Memory: 7.22 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.py
@@ -0,0 +1,11 @@
+# def aa(string):
+#     str_list=list(str)
+#     str2 = ''.join(str_list[::-1])
+#     return
+# str = input()
+# rotate_string=rotate_string(str2)
+# print(rotate_string)
+str = input()
+# print(str\n)
+for str in str:
+    print(str)
